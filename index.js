00c08249f62c65e6b2881a5b041909d423cbdaa6
shafna96 <sfnmail2@gmail.com>
2021-06-11 12:12:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+const fruits = ['apple','oranges','mango','stawberry','grapes']
+const fruits1 = fruits.slice(0,3)
+fruits.reverse();
+fruits.splice(3,1)
+
+console.log(fruits)
+console.log(fruits1)
