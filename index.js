1634ad146be9f303270481c2ad43e7cbbdbac389
shafna96 <sfnmail2@gmail.com>
2021-06-14 12:11:35 +0530

    button Added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 
 
-const fruits = ['apple','oranges','mango','stawberry','grapes']
-const fruits1 = fruits.slice(0,3)
+const fruits = ['apple', 'oranges', 'mango', 'stawberry', 'grapes']
+const fruits1 = fruits.slice(0, 3)
 fruits.reverse();
-fruits.splice(3,1)
+fruits.splice(3, 1)
+const button = document.querySelector("button")
+button.addEventListener('click', showAlert)
+
+function showAlert() {
+    alert(fruits.toString());
+}
 
 // console.log(fruits)
 // console.log(fruits1)
